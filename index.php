5f37d0f47b8a1f24f7ca6b12ee82e6f363b9be75
moe-developerr <moe.developerr@gmail.com>
2017-03-24 18:47:40 +0200

    build aside and main slider

--- index.php
+++ index.php
@@ -7,13 +7,61 @@
 	<link rel="stylesheet" href="/dist/styles/styles.css">
 </head>
 <body>
-<div class="site-wrapper">
-	<?php include './includes/top-header.php'; ?>
-	<?php include './includes/aside.php'; ?>
-	<main class="main">
-		main
-	</main>
-	<?php include './includes/footer.php'; ?>
-</div>
+	<div class="site-wrapper">
+		<?php include './includes/top-header.php'; ?>
+		<?php include './includes/aside.php'; ?>
+		<main class="main">
+			<div class="slider light-arrows full-height-slider rsDefault">
+				<div class="slide rsContent">
+					<a class="rsImg" href="/src/images/top-slider-home.jpg"></a>
+				</div>
+				<div class="slide rsContent">
+					<a class="rsImg" href="/src/images/top-slider-home.jpg"></a>
+				</div>
+			</div>
+			<section>
+				<div class="line-through-title margin-bot">
+					<hr class="line">
+					<div class="titles">
+						<div class="title">WE DON'T DEVELOP HOUSES, WE DESIGN HOMES</div>
+						<div class="subtitle">A house is made of bricks While a Home is built with Love and Dreams</div>
+					</div>
+				</div>
+				<div class="centered-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, possimus libero quibusdam voluptate sapiente repellendus enim, eveniet saepe aut sed ea velit, odit officia fugit molestias vel laboriosam, numquam quaerat voluptatibus doloremque maxime maiores? Accusamus, illo ducimus laborum quos, nihil assumenda natus alias dolores debitis, illum a quas ab perspiciatis.</div>
+			</section>
+			<section>
+				<div class="line-through-title margin-bot">
+					<hr class="line">
+					<div class="titles">
+						<div class="title">OUR LATEST INSPIRATIONS</div>
+						<div class="subtitle">Designed to Perfection</div>
+					</div>
+				</div>
+<!-- 				<div class="slider dark-arrows visible-nearby-slider rsDefault">
+					<div class="slide rsContent">
+						<a class="rsImg" href="/src/images/top-slider-home.jpg"></a>
+						<div class="slide-title">PRIDE OF MAR TAKLA II</div>
+						<div class="slide-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, minus!</div>
+					</div>
+					<div class="slide rsContent">
+						<a class="rsImg" href="/src/images/top-slider-home.jpg"></a>
+						<div class="slide-title">PRIDE OF MAR TAKLA II</div>
+						<div class="slide-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, ipsam?</div>
+					</div>
+					<div class="slide rsContent">
+						<a class="rsImg" href="/src/images/top-slider-home.jpg"></a>
+						<div class="slide-title">PRIDE OF MAR TAKLA II</div>
+						<div class="slide-text">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, consectetur.
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, consectetur.
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, consectetur.
+						</div>
+					</div>
+				</div> -->
+			</section>
+		</main>
+		<?php include './includes/footer.php'; ?>
+		<?php include './includes/scripts.php'; ?>
+	</div>
 </body>
 </html>
